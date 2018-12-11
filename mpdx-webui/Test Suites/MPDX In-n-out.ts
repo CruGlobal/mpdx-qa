001eb0ba8894b214ad7d31ed11b784b8ede25bcb
BobBuford <bob.buford@cru.org>
2018-12-11 17:19:49 -0500

    update web project setings

--- mpdx-webui/Test Suites/MPDX In-n-out.ts
+++ mpdx-webui/Test Suites/MPDX In-n-out.ts
@@ -4,7 +4,7 @@
    <name>MPDX In-n-out</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-11-26T16:57:19</lastRun>
+   <lastRun>2018-12-11T16:54:24</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
